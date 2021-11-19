a92dab5cd354bd81e95982b4ef835d45dd7598e4
chaisuresh <chaithra.suresh@colorado.edu>
2021-11-18 18:36:18 -0700

    build worked

--- base_external/package/lcd-test/lcd-test.mk
+++ base_external/package/lcd-test/lcd-test.mk
@@ -6,22 +6,21 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 0d3bef4ad5cbfe62af57647fb574f985bc4f8e90
+LCD_TEST_VERSION = 1364a3601b488c8b1e79ba3bd100f0889c24f15f 
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-LCD_TEST_SITE = git@github.com:chaisuresh/lcd_test.git
+LCD_TEST_SITE = git@github.com:cu-ecen-aeld/final-project-chaithra-sharath.git
 LCD_TEST_SITE_METHOD = git
 LCD_TEST_GIT_SUBMODULES = YES
 
 define LCD_TEST_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/ArmbianIO all
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/LCD-nokia5110 all
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define LCD_TEST_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/n5110 $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/LCD-nokia5110/* $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
